ec588a5c01ceea087ee6b3f9396a312b83494387
Sebastian Köhler <sk@tyrion.de>
2025-11-11 10:32:06 +0100

    fix typo

--- build/entrypoint.sh
+++ build/entrypoint.sh
@@ -4,15 +4,15 @@
 : "${USERMAP_GID:=0}"
 
 if [ ${USERMAP_UID} -ne 1000 ] && [ ${USERMAP_UID} -ne 0 ]; then
-    usermod --non-unique --uid "${USERMAP_UID}" sync
+    usermod --non-unique --uid "${USERMAP_UID}" isync
 fi
 
 if [ ${USERMAP_GID} -ne 1000 ] && [ ${USERMAP_GID} -ne 0 ]; then
-    groupmod --non-unique --gid "${USERMAP_GID}" sync
+    groupmod --non-unique --gid "${USERMAP_GID}" isync
 fi
 
 if [ ${USERMAP_UID} -ne 0 ]; then 
-    gosu sync /usr/local/bin/mbsync -a -V -c /config/isyncrc
+    gosu isync /usr/local/bin/mbsync -a -V -c /config/isyncrc
 else
     /usr/local/bin/mbsync -a -V -c /config/isyncrc
 fi
@@ -20,7 +20,7 @@ fi
 
 if [ ! -f "/etc/cron.d/mbsync" ]; then
     if [ "${USERMAP_UID}" -ne 0 ]; then
-        echo "30 1 * * * sync /usr/local/bin/mbsync -a -V -c /config/isyncrc > /proc/1/fd/1 2> /proc/1/fd/2" > /etc/cron.d/mbsync
+        echo "30 1 * * * isync /usr/local/bin/mbsync -a -V -c /config/isyncrc > /proc/1/fd/1 2> /proc/1/fd/2" > /etc/cron.d/mbsync
     else
         echo "30 1 * * * root /usr/local/bin/mbsync -a -V -c /config/isyncrc > /proc/1/fd/1 2> /proc/1/fd/2" > /etc/cron.d/mbsync
     fi
